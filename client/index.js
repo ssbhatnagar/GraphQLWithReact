add154339dfb8fcf40d45fd46ddf9be793059119
Bhatnagar <2063038@cognizant.com>
2024-08-25 02:47:13 +0530

    feat: added react router

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
+import App from './components/App';
 import { ApolloProvider } from 'react-apollo';
 
 import SongList from './components/SongList';
@@ -10,7 +12,11 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
   <ApolloProvider client={client}>
-    <SongList/>
+    <Router history={hashHistory}>
+      <Route path="/" component = {App}>
+        <IndexRoute component = {SongList}/>
+      </Route>
+    </Router>
   </ApolloProvider>
   );
 };
