e0f5bde61841815cad9c1044384ccd747e30fa83
Bhatnagar <2063038@cognizant.com>
2024-08-25 01:23:17 +0530

    feat: handeling pending queries

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
-import {graphql} from 'react-apollo';
+import { graphql } from "react-apollo";
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map((song) => {
+      return <li>{song.title}</li>;
+    });
+  }
+
   render() {
-    console.log(this.props);
-    return <div>SongList</div>;
+    if (this.props.data.loading) {
+      return <div>Loading .....</div>;
+    }
+    return <div>{this.renderSongs()}</div>;
   }
 }
 
