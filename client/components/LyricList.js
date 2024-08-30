1533a8ba911abbaeeb316daa718483ff731177be
S S Bhatnagar <shubhambhatnagar072@gmail.com>
2024-08-31 01:28:38 +0530

    fix: handling optimistic responses

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,22 @@
 import React, {Component} from "react";
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
+import { optimistic } from "apollo-client/optimistic-data/store";
  
 class LyricList extends Component{
 
-    onLike(id){
-        this.props.mutate({variables: {id}})
+    onLike(id, likes){
+        this.props.mutate({
+            variables: {id},
+            optimisticResponse:{
+                __typename: 'Mutation',
+                likeLyric:{
+                    id,
+                    __typename:'LyricType',
+                    likes: likes + 1
+                }
+            }
+        });
     }
 
     renderLyrics(){
@@ -14,7 +25,7 @@ class LyricList extends Component{
                 <li key={id} className="collection-item">
                     {content}
                     <div className="vote-box">
-                    <i className="material-icons" onClick={()=> this.onLike(id)}>thumb_up</i>
+                    <i className="material-icons" onClick={()=> this.onLike(id, likes)}>thumb_up</i>
                     {likes}
                     </div>
                 </li>
